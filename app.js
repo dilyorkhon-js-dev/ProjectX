bf2aebdec87a3bd2c1a976c938a27f6ed79f1709
dilyorkhon-js-dev <dilyorzizi@gmail.com>
2023-12-25 23:52:39 +0500

    added changes

---
+++ app.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: 3,
+  delay: 2000,
+  pauseOnMouseEnter: true,
+  spaceBetween: 20,
+  disableOnInteraction: true,
+});
+swiper.start();
